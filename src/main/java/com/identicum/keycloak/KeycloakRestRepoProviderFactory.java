e69d9e669e81758c3da012fc217147efbe5fc899
Ezequiel Sandoval <sandovalezequiel@gmail.com>
2021-10-15 18:47:00 +0300

    Dynamic httpStatus (#7)
    * code formatting
    
    * #5 added httpStatus
    
    Co-authored-by: Ezequiel Sandoval <ezequielsandoval@Ezequiels-MacBook-Pro.local>

--- src/main/java/com/identicum/keycloak/KeycloakRestRepoProviderFactory.java
+++ src/main/java/com/identicum/keycloak/KeycloakRestRepoProviderFactory.java
@@ -1,6 +1,7 @@
 package com.identicum.keycloak;
 
 import org.jboss.logging.Logger;
+import org.keycloak.Config.Scope;
 import org.keycloak.common.util.MultivaluedHashMap;
 import org.keycloak.component.ComponentModel;
 import org.keycloak.component.ComponentValidationException;
@@ -11,97 +12,122 @@ import org.keycloak.provider.ProviderConfigurationBuilder;
 import org.keycloak.storage.UserStorageProviderFactory;
 
 import java.util.List;
+import java.util.Timer;
+
+import static com.identicum.keycloak.HttpStats.TO_MILLISECONDS;
+import static com.identicum.keycloak.RestConfiguration.API_CONNECTION_REQUEST_TIMEOUT;
+import static com.identicum.keycloak.RestConfiguration.API_CONNECT_TIMEOUT;
+import static com.identicum.keycloak.RestConfiguration.API_SOCKET_TIMEOUT;
+import static com.identicum.keycloak.RestConfiguration.AUTH_BASIC;
+import static com.identicum.keycloak.RestConfiguration.AUTH_NONE;
+import static com.identicum.keycloak.RestConfiguration.AUTH_OAUTH;
+import static com.identicum.keycloak.RestConfiguration.HTTP_STATS_INTERVAL;
+import static com.identicum.keycloak.RestConfiguration.PROPERTY_AUTH_TYPE;
+import static com.identicum.keycloak.RestConfiguration.PROPERTY_BASE_URL;
+import static com.identicum.keycloak.RestConfiguration.PROPERTY_BASIC_PASSWORD;
+import static com.identicum.keycloak.RestConfiguration.PROPERTY_BASIC_USERNAME;
+import static com.identicum.keycloak.RestConfiguration.PROPERTY_MAX_HTTP_CONNECTIONS;
+import static com.identicum.keycloak.RestConfiguration.PROPERTY_OAUTH_CLIENT_ID;
+import static com.identicum.keycloak.RestConfiguration.PROPERTY_OAUTH_CLIENT_SECRET;
+import static com.identicum.keycloak.RestConfiguration.PROPERTY_OAUTH_SCOPE;
+import static com.identicum.keycloak.RestConfiguration.PROPERTY_OAUTH_TOKEN_ENDPOINT;
+import static com.identicum.keycloak.RestConfiguration.validate;
+import static org.jboss.logging.Logger.getLogger;
+import static org.keycloak.provider.ProviderConfigProperty.LIST_TYPE;
+import static org.keycloak.provider.ProviderConfigProperty.PASSWORD;
+import static org.keycloak.provider.ProviderConfigProperty.STRING_TYPE;
 
 public class KeycloakRestRepoProviderFactory implements UserStorageProviderFactory<KeycloakRestRepoProvider> {
 
-	private static final Logger logger = Logger.getLogger(KeycloakRestRepoProviderFactory.class);
-	protected static final List<ProviderConfigProperty> configMetadata;
+	private static final Logger logger = getLogger(KeycloakRestRepoProviderFactory.class);
+	private List<ProviderConfigProperty> configMetadata;
 
 	private MultivaluedHashMap<String, String> lastConfiguration = null;
 
-	static {
+	private RestHandler restHandler;
+	private Timer httpStats;
+
+	@Override
+	public void init(Scope config) {
 		ProviderConfigurationBuilder builder = ProviderConfigurationBuilder.create();
-		builder.property().name(RestConfiguration.PROPERTY_BASE_URL)
-			.type(ProviderConfigProperty.STRING_TYPE).label("Base URL")
+		builder.property().name(PROPERTY_BASE_URL)
+			.type(STRING_TYPE).label("Base URL")
 			.defaultValue("http://rest-users-api:8081/")
 			.helpText("Api url base to authenticate users")
 			.add();
-		builder.property().name(RestConfiguration.PROPERTY_MAX_HTTP_CONNECTIONS)
-			.type(ProviderConfigProperty.STRING_TYPE).label("Max pool connections")
+		builder.property().name(PROPERTY_MAX_HTTP_CONNECTIONS)
+			.type(STRING_TYPE).label("Max pool connections")
 			.defaultValue("5")
 			.helpText("Max http connections in pool")
 			.add();
-		builder.property().name(RestConfiguration.PROPERTY_AUTH_TYPE)
-			.type(ProviderConfigProperty.LIST_TYPE).label("Api Authorization")
-			.options(RestConfiguration.AUTH_NONE, RestConfiguration.AUTH_BASIC, RestConfiguration.AUTH_OAUTH)
-			.defaultValue(RestConfiguration.AUTH_NONE)
+		builder.property().name(PROPERTY_AUTH_TYPE)
+			.type(LIST_TYPE).label("Api Authorization")
+			.options(AUTH_NONE, AUTH_BASIC, AUTH_OAUTH)
+			.defaultValue(AUTH_NONE)
 			.helpText("Authorization method used by consumed API")
 			.add();
-		builder.property().name(RestConfiguration.PROPERTY_OAUTH_TOKEN_ENDPOINT)
-			.type(ProviderConfigProperty.STRING_TYPE).label("OAuth2 Token Endpoint")
+		builder.property().name(PROPERTY_OAUTH_TOKEN_ENDPOINT)
+			.type(STRING_TYPE).label("OAuth2 Token Endpoint")
 			.defaultValue("http://localhost:8080/auth/realms/restrepo/protocol/openid-connect/token")
 			.helpText("Endpoint to negotiate the token with client_credentials grant type (required for OAUTH authorization)")
 			.add();
-		builder.property().name(RestConfiguration.PROPERTY_OAUTH_CLIENT_ID)
-			.type(ProviderConfigProperty.STRING_TYPE).label("OAuth2 Client Id")
+		builder.property().name(PROPERTY_OAUTH_CLIENT_ID)
+			.type(STRING_TYPE).label("OAuth2 Client Id")
 			.defaultValue("")
 			.helpText("client_id to negotiate the Access Token (required for OAUTH authorization)")
 			.add();
-		builder.property().name(RestConfiguration.PROPERTY_OAUTH_CLIENT_SECRET)
-			.type(ProviderConfigProperty.PASSWORD).label("OAuth2 Client Secret")
+		builder.property().name(PROPERTY_OAUTH_CLIENT_SECRET)
+			.type(PASSWORD).label("OAuth2 Client Secret")
 			.defaultValue("")
 			.helpText("client_secret to negotiate the Access Token (required for OAUTH authorization)")
 			.add();
-		builder.property().name(RestConfiguration.PROPERTY_OAUTH_SCOPE)
-			.type(ProviderConfigProperty.STRING_TYPE).label("OAuth2 Scope")
+		builder.property().name(PROPERTY_OAUTH_SCOPE)
+			.type(STRING_TYPE).label("OAuth2 Scope")
 			.defaultValue("")
 			.helpText("Required scope in the access_token request")
 			.add();
-		builder.property().name(RestConfiguration.PROPERTY_BASIC_USERNAME)
-			.type(ProviderConfigProperty.STRING_TYPE).label("Auth Basic Username")
+		builder.property().name(PROPERTY_BASIC_USERNAME)
+			.type(STRING_TYPE).label("Auth Basic Username")
 			.defaultValue("")
 			.helpText("Username used for Basic Authentication")
 			.add();
-		builder.property().name(RestConfiguration.PROPERTY_BASIC_PASSWORD)
-			.type(ProviderConfigProperty.PASSWORD).label("Auth Basic Password")
+		builder.property().name(PROPERTY_BASIC_PASSWORD)
+			.type(PASSWORD).label("Auth Basic Password")
 			.defaultValue("")
 			.helpText("Password used for Basic Authentication")
 			.add();
-		builder.property().name(RestConfiguration.PROPERTY_STATS_ENABLED)
-			.type(ProviderConfigProperty.LIST_TYPE).label("HTTP pool stats enabled")
-			.options(RestConfiguration.STATS_ENABLED_YES, RestConfiguration.STATS_ENABLED_NO)
-			.defaultValue(RestConfiguration.STATS_ENABLED_NO)
-			.helpText("Log HTTP pool stats in repo-provider initialization?")
-			.add();
-		builder.property().name(RestConfiguration.API_SOCKET_TIMEOUT)
-			.type(ProviderConfigProperty.STRING_TYPE).label("API Socket Timeout")
+		builder.property().name(API_SOCKET_TIMEOUT)
+			.type(STRING_TYPE).label("API Socket Timeout")
 			.defaultValue("1000")
 			.add();
-		builder.property().name(RestConfiguration.API_CONNECT_TIMEOUT)
-			.type(ProviderConfigProperty.STRING_TYPE).label("API Connect Timeout")
+		builder.property().name(API_CONNECT_TIMEOUT)
+			.type(STRING_TYPE).label("API Connect Timeout")
 			.defaultValue("1000")
 			.add();
-		builder.property().name(RestConfiguration.API_CONNECTION_REQUEST_TIMEOUT)
-			.type(ProviderConfigProperty.STRING_TYPE).label("API Connection Request Timeout")
+		builder.property().name(API_CONNECTION_REQUEST_TIMEOUT)
+			.type(STRING_TYPE).label("API Connection Request Timeout")
 			.defaultValue("1000")
 			.add();
+		builder.property().name(HTTP_STATS_INTERVAL)
+				.type(STRING_TYPE).label("HttpStats in secs")
+				.defaultValue("0")
+				.add();
 		configMetadata = builder.build();
+		this.httpStats = new Timer();
 	}
-
-	private RestHandler restHandler;
 
 	@Override
 	public KeycloakRestRepoProvider create(KeycloakSession session, ComponentModel model) {
-		if(this.restHandler == null || !model.getConfig().equals( this.lastConfiguration )) {
+		if(restHandler == null || !model.getConfig().equals( lastConfiguration )) {
 			logger.infov("Creating a new instance of restHandler");
-			RestConfiguration configuration = new RestConfiguration(model.getConfig());
-			this.restHandler = new RestHandler(configuration);
-			this.lastConfiguration = model.getConfig();
-		}
-		else {
+			RestConfiguration restConfiguration = new RestConfiguration(model.getConfig());
+			restHandler = new RestHandler(restConfiguration);
+			setHttpStats(logger, restHandler, restConfiguration.getHttpStatsInterval());
+			lastConfiguration = model.getConfig();
+		} else {
 			logger.infov("RestHandler already instantiated");
 		}
-		return new KeycloakRestRepoProvider(session, model, this.restHandler);
+		return new KeycloakRestRepoProvider(session, model, restHandler);
 	}
 
 	@Override
@@ -116,6 +142,14 @@ public class KeycloakRestRepoProviderFactory implements UserStorageProviderFacto
 	
 	@Override
 	public void validateConfiguration(KeycloakSession session, RealmModel realm, ComponentModel config) throws ComponentValidationException {
-		RestConfiguration.validate(config.getConfig());
+		validate(config.getConfig());
+	}
+
+	private void setHttpStats(Logger logger, RestHandler restHandler, Integer httpStatsInterval){
+		httpStats.cancel();
+		if(httpStatsInterval > 0){
+			this.httpStats = new Timer();
+			this.httpStats.schedule(new HttpStats(logger, restHandler), 0, httpStatsInterval * TO_MILLISECONDS);
+		}
 	}
 }
--- src/main/java/com/identicum/keycloak/SimpleHttpResponse.java
+++ src/main/java/com/identicum/keycloak/SimpleHttpResponse.java
@@ -1,52 +1,37 @@
 package com.identicum.keycloak;
 
-import java.io.StringReader;
-import javax.json.Json;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
 import javax.json.JsonArray;
 import javax.json.JsonObject;
 import javax.json.JsonReader;
+import java.io.StringReader;
 
+import static javax.json.Json.createReader;
+
+@Getter
+@AllArgsConstructor
 public class SimpleHttpResponse {
 
 	private int status;
 	private String response;
 
-	public SimpleHttpResponse(int status, String body) {
-		this.status = status;
-		this.response = body;
-	}
-
-	public int getStatus() {
-		return status;
-	}
-
 	public boolean isSuccess(){
 		return status == 200;
 	}
 
-	public void setStatus(int status) {
-		this.status = status;
-	}
-
-	public String getResponse() {
-		return response;
-	}
-
-	public void setResponse(String response) {
-		this.response = response;
-	}
-
 	public JsonObject getResponseAsJsonObject() {
-		if(this.response != null) {
-			JsonReader reader = Json.createReader(new StringReader(response));
+		if(response != null) {
+			JsonReader reader = createReader(new StringReader(response));
 			return reader.readObject();
 		}
 		return null;
 	}
 
 	public JsonArray getResponseAsJsonArray() {
-		if(this.response != null) {
-			JsonReader reader = Json.createReader(new StringReader(response));
+		if(response != null) {
+			JsonReader reader = createReader(new StringReader(response));
 			return reader.readArray();
 		}
 		return null;

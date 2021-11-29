af5988f6398bce347529f26a0deb7acf89a26d03
osorionicolas <osorionicolas95@gmail.com>
2021-11-29 16:13:44 -0300

    Replaced RuntimeExceptions with ForkFlowExceptions. Updated pom.xml. Fixed issue with profile validation. (#8)
    * Replaced RuntimeExceptions with ForkFlowExceptions. Updated pom.xml. Fixed issue with profile validation.
    
    * Replaced RuntimeExceptions with ForkFlowExceptions. Updated pom.xml. Fixed issue with profile validation.
    
    * updated implementation version
    
    * updated github action
    
    * removed unused statsEnabled

--- src/main/java/com/identicum/keycloak/KeycloakRestRepoProviderFactory.java
+++ src/main/java/com/identicum/keycloak/KeycloakRestRepoProviderFactory.java
@@ -49,68 +49,70 @@ public class KeycloakRestRepoProviderFactory implements UserStorageProviderFacto
 
 	@Override
 	public void init(Scope config) {
+		logger.infov("Initializing Keycloak Rest Repo factory version: " + getClass().getPackage().getImplementationVersion());
+
 		ProviderConfigurationBuilder builder = ProviderConfigurationBuilder.create();
 		builder.property().name(PROPERTY_BASE_URL)
-			.type(STRING_TYPE).label("Base URL")
-			.defaultValue("http://rest-users-api:8081/")
-			.helpText("Api url base to authenticate users")
-			.add();
+				.type(STRING_TYPE).label("Base URL")
+				.defaultValue("http://rest-users-api:8081/")
+				.helpText("Api url base to authenticate users")
+				.add();
 		builder.property().name(PROPERTY_MAX_HTTP_CONNECTIONS)
-			.type(STRING_TYPE).label("Max pool connections")
-			.defaultValue("5")
-			.helpText("Max http connections in pool")
-			.add();
+				.type(STRING_TYPE).label("Max pool connections")
+				.defaultValue("5")
+				.helpText("Max http connections in pool")
+				.add();
 		builder.property().name(PROPERTY_AUTH_TYPE)
-			.type(LIST_TYPE).label("Api Authorization")
-			.options(AUTH_NONE, AUTH_BASIC, AUTH_OAUTH)
-			.defaultValue(AUTH_NONE)
-			.helpText("Authorization method used by consumed API")
-			.add();
+				.type(LIST_TYPE).label("Api Authorization")
+				.options(AUTH_NONE, AUTH_BASIC, AUTH_OAUTH)
+				.defaultValue(AUTH_NONE)
+				.helpText("Authorization method used by consumed API")
+				.add();
 		builder.property().name(PROPERTY_OAUTH_TOKEN_ENDPOINT)
-			.type(STRING_TYPE).label("OAuth2 Token Endpoint")
-			.defaultValue("http://localhost:8080/auth/realms/restrepo/protocol/openid-connect/token")
-			.helpText("Endpoint to negotiate the token with client_credentials grant type (required for OAUTH authorization)")
-			.add();
+				.type(STRING_TYPE).label("OAuth2 Token Endpoint")
+				.defaultValue("http://localhost:8080/auth/realms/restrepo/protocol/openid-connect/token")
+				.helpText("Endpoint to negotiate the token with client_credentials grant type (required for OAUTH authorization)")
+				.add();
 		builder.property().name(PROPERTY_OAUTH_CLIENT_ID)
-			.type(STRING_TYPE).label("OAuth2 Client Id")
-			.defaultValue("")
-			.helpText("client_id to negotiate the Access Token (required for OAUTH authorization)")
-			.add();
+				.type(STRING_TYPE).label("OAuth2 Client Id")
+				.defaultValue("")
+				.helpText("client_id to negotiate the Access Token (required for OAUTH authorization)")
+				.add();
 		builder.property().name(PROPERTY_OAUTH_CLIENT_SECRET)
-			.type(PASSWORD).label("OAuth2 Client Secret")
-			.defaultValue("")
-			.helpText("client_secret to negotiate the Access Token (required for OAUTH authorization)")
-			.add();
+				.type(PASSWORD).label("OAuth2 Client Secret")
+				.defaultValue("")
+				.helpText("client_secret to negotiate the Access Token (required for OAUTH authorization)")
+				.add();
 		builder.property().name(PROPERTY_OAUTH_SCOPE)
-			.type(STRING_TYPE).label("OAuth2 Scope")
-			.defaultValue("")
-			.helpText("Required scope in the access_token request")
-			.add();
+				.type(STRING_TYPE).label("OAuth2 Scope")
+				.defaultValue("")
+				.helpText("Required scope in the access_token request")
+				.add();
 		builder.property().name(PROPERTY_BASIC_USERNAME)
-			.type(STRING_TYPE).label("Auth Basic Username")
-			.defaultValue("")
-			.helpText("Username used for Basic Authentication")
-			.add();
+				.type(STRING_TYPE).label("Auth Basic Username")
+				.defaultValue("")
+				.helpText("Username used for Basic Authentication")
+				.add();
 		builder.property().name(PROPERTY_BASIC_PASSWORD)
-			.type(PASSWORD).label("Auth Basic Password")
-			.defaultValue("")
-			.helpText("Password used for Basic Authentication")
-			.add();
+				.type(PASSWORD).label("Auth Basic Password")
+				.defaultValue("")
+				.helpText("Password used for Basic Authentication")
+				.add();
 		builder.property().name(API_SOCKET_TIMEOUT)
-			.type(STRING_TYPE).label("API Socket Timeout")
-			.defaultValue("1000")
-			.helpText("Max time [milliseconds] to wait for response")
-			.add();
+				.type(STRING_TYPE).label("API Socket Timeout")
+				.defaultValue("1000")
+				.helpText("Max time [milliseconds] to wait for response")
+				.add();
 		builder.property().name(API_CONNECT_TIMEOUT)
-			.type(STRING_TYPE).label("API Connect Timeout")
-			.defaultValue("1000")
-			.helpText("Max time [milliseconds] to establish the connection")
-			.add();
+				.type(STRING_TYPE).label("API Connect Timeout")
+				.defaultValue("1000")
+				.helpText("Max time [milliseconds] to establish the connection")
+				.add();
 		builder.property().name(API_CONNECTION_REQUEST_TIMEOUT)
-			.type(STRING_TYPE).label("API Connection Request Timeout")
-			.defaultValue("1000")
-			.helpText("Max time [milliseconds] to wait until a connection in the pool is assigned to the requesting thread")
-			.add();
+				.type(STRING_TYPE).label("API Connection Request Timeout")
+				.defaultValue("1000")
+				.helpText("Max time [milliseconds] to wait until a connection in the pool is assigned to the requesting thread")
+				.add();
 		builder.property().name(HTTP_STATS_INTERVAL)
 				.type(STRING_TYPE).label("HttpStats interval")
 				.defaultValue("0")
@@ -143,7 +145,7 @@ public class KeycloakRestRepoProviderFactory implements UserStorageProviderFacto
 	public List<ProviderConfigProperty> getConfigProperties() {
 		return configMetadata;
 	}
-	
+
 	@Override
 	public void validateConfiguration(KeycloakSession session, RealmModel realm, ComponentModel config) throws ComponentValidationException {
 		validate(config.getConfig());
